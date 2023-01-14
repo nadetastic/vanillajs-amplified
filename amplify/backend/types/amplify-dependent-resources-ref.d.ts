ac6f9bc5689d98b6d4e54bca127aaf054b7bba55
Dan Kiuna <dkkiuna11@gmail.com>
2023-01-14 02:59:37 -0600

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,16 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "authemailusernameb2b52776b2b52776": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "GoogleWebClient": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
