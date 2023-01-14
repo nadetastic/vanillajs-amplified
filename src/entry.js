600a1890e4de40e0748ebbad6f622a9f030c190e
Dan Kiuna <dkkiuna11@gmail.com>
2023-01-14 03:01:20 -0600

    initial commit

--- src/entry.js
+++ src/entry.js
@@ -1,16 +1,5 @@
-import { Amplify, Auth } from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
 
 console.log(Amplify.configure());
-
-const signIn = async () => {
-    try {
-        const user = await Auth.signIn('dkkiuna11@gmail.com', 'abcd1234');
-        console.log(user);
-    } catch (error) {
-        console.log('error signing in', error);
-    }
-}
-
-signIn();
