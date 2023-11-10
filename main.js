bfca4241482b6f6bd44ecc4492ad44f53cfc7d7a
Dan Kiuna <dkkiuna11@gmail.com>
2023-11-09 18:33:07 -0600

    init

---
+++ main.js
@@ -0,0 +1,25 @@
+import { Amplify } from 'aws-amplify';
+import { generateClient } from 'aws-amplify/api';
+
+// import awsconfig from './aws-exports';
+// import { createTodo } from './graphql/mutations';
+
+// Amplify.configure(awsconfig);
+
+async function createNewTodo() {
+  const todo = {
+    name: 'Use AppSync',
+    description: `Realtime and Offline (${new Date().toLocaleString()})`
+  };
+
+  return await API.graphql(graphqlOperation(createTodo, { input: todo }));
+}
+
+const MutationButton = document.getElementById('MutationEventButton');
+const MutationResult = document.getElementById('MutationResult');
+
+MutationButton.addEventListener('click', (evt) => {
+  createNewTodo().then((evt) => {
+    MutationResult.innerHTML += `<p>${evt.data.createTodo.name} - ${evt.data.createTodo.description}</p>`;
+  });
+});
